f0d8805fc041af7e9763663190ed455db827c85c
Ken Kaiser <kkaiser@doublehelixllc.com>
2016-05-04 12:59:59 -0700

    setting up more testing with nested describe and before each functions to add additional tests in component

--- src/components/comment_box.js
+++ src/components/comment_box.js
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 
 export default class CommentBox extends Component {
   render(){
-    console.log(CommentBox)
+    
     return (
       <div className = "comment-box">
         <textarea/>
--- test/components/app_test.js
+++ test/components/app_test.js
@@ -2,12 +2,13 @@ import { renderComponent , expect } from '../test_helper';
 import App from '../../src/components/app';
 
 describe('App' , () => {
+  let component;
 
+  beforeEach(()=>{
+    component  = renderComponent(App);
+  });
 
-  it('shows the correct text', () => {
-    // create an instance of app
-    const component  = renderComponent(App);
-    
-    expect(component).to.contain('React simple starter');
+  it('shows a comment box', ()=>{
+    expect(component.find('.comment-box')).to.exist;
   });
 });
